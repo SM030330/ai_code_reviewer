4ba9ef89373a168213075184fc802b87cc0e1177
SM030330 <masamasa030330@gmail.com>
2025-02-02 22:46:49 +0900

    tailwindcssの足りないpkのinstall

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// filepath: /Users/shojimasaki/Desktop/udemy_react/ai_code_reviewer/ai_code_reviewer/tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

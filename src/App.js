5a04125c838ee5f727bc823d6b078a1a5d1ddb9d
SM030330 <masamasa030330@gmail.com>
2025-02-02 23:27:02 +0900

    open ai responseのmarkdownのデザイン修正

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Markdown from 'react-markdown';
 import openai from './lib/openai';
 
 const prompt = `
@@ -14,8 +15,10 @@ const prompt = `
 function App() {
   const [content, setContent] = useState('');
   const [result, setResult] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const review = async () => {
+    setIsLoading(true);
     const messages = [
       {
         role: 'user',
@@ -25,6 +28,7 @@ function App() {
 
     const result = await openai.completion(messages);
     setResult(result);
+    setIsLoading(false);
   };
 
   return (
@@ -44,13 +48,16 @@ function App() {
           </div>
           <button
             onClick={review}
+            disabled={isLoading}
             className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
           >
-            レビューする
+            {isLoading ? 'レビュー中' : 'レビューする'}
           </button>
         </div>
         <div className="flex flex-col w-1/2 h-full items-center justify-center">
-          <div className="p-4 overflow-y-auto w-full">{result}</div>
+          <div className="p-4 overflow-y-auto w-full">
+            {isLoading ? 'レビュー中...' : <Markdown className="markdown">{result}</Markdown>}
+          </div>
         </div>
       </main>
     </div>
